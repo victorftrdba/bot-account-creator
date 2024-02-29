417e5c375133bfbbc2b45205022822dc91abf084
Victor Nogueira <69355100+victorftrdba@users.noreply.github.com>
2024-02-29 11:01:14 -0300

    Trocando readline para rlSync

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 process.on('uncaughtException', () => { });
 
-const readline = require('readline');
 const {
   createAccountsWithSelenium,
   generateName
 } = require('./functions.js');
 const rlSync = require('readline-sync');
-
-const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
 
 (async () => {
   const link = rlSync.question('Informe o link da plataforma: \n', {
@@ -22,10 +19,10 @@ const rl = readline.createInterface({ input: process.stdin, output: process.stdo
 
   console.log(`Iniciando criação de contas para ${link} ${isWithCpf ? 'com CPF' : 'sem CPF'}`)
 
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < 2; i++) {
     await createAccountsWithSelenium(generateName(), link, isWithCpf)
   }
 
   // setInterval(() => openVpn('br.protonvpn.tcp.ovpn'), 60000)
-  rl.question('Encerrar script, aperte ENTER', () => process.exit());
+  rlSync.question('Encerrar script, aperte ENTER');
 })();
