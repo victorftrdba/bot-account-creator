123ba20593fdef9efd23ff02d4b1158be738dc6f
Victor Nogueira <victornogu80@gmail.com>
2024-06-11 12:04:26 -0300

    Echo na instalação

--- instalacao/instalar.sh
+++ instalacao/instalar.sh
@@ -1,5 +1,8 @@
 cd ..
+echo "Instalando arquivos do script..."
 npm i
+echo "Instalando comandos importantes..."
 npm i -g typescript
 npm i -g ts-node
+echo "Instalando navegador..."
 npx puppeteer install browsers chrome
